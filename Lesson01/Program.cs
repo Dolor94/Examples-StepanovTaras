70e50c68718495c4a7144a332d77273b3659b1ee
Dolor <taras_stepanov@mail.ru>
2022-06-24 13:06:14 +0500

    Homework - Task №2

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,9 +1,20 @@
-﻿// Первые шаги:
-Console.WriteLine("Hello, World and Geekbrains!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// Привествуем Пользователя:
-Console.Write("Ввдите ваше Имя: ");
-string username = Console.ReadLine();
-Console.Write("Введите вашу Фамилию: ");
-string usersurname = Console.ReadLine();
-Console.Write("Привет " + usersurname + " " + username + "!");
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write ("Введите первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+double max_number = number1;
+double min_number = number2;
+
+if (number2 > number1)
+{
+    max_number = number2;
+    min_number = number1;
+}
+Console.WriteLine("Наибольшим числом является число: " + (max_number));
+Console.WriteLine("Наименьшим числом является число: " + (min_number));
