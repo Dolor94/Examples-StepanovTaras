0cd51f2a4408eac0b627c6fd282e6830e5dc5273
Dolor <taras_stepanov@mail.ru>
2022-07-02 14:39:17 +0500

    Homework - Task №3

--- Practical task №2/Lesson02/Program.cs
+++ Practical task №2/Lesson02/Program.cs
@@ -4,18 +4,44 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-string numText = Convert.ToString(num);
-if (numText.Length < 3)
-{
-    Console.WriteLine("У вашего числа нет третьей цифры");
-}
-else if (num < 0)
-    {
-        Console.WriteLine($"Третьей цифрой числа {num} является цифра {numText[3]}");
-    }
-else Console.WriteLine($"Третьей цифрой числа {num} является цифра {numText[2]}");
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length < 3)
+// {
+//     Console.WriteLine("У вашего числа нет третьей цифры");
+// }
+// else if (num < 0)
+//     {
+//         Console.WriteLine($"Третьей цифрой числа {num} является цифра {numText[3]}");
+//     }
+// else Console.WriteLine($"Третьей цифрой числа {num} является цифра {numText[2]}");
 
 
 // Добавил условие если пользователь введёт отрицательное число.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int figure3 = 0;
+if (num > 999)
+{
+    while (num > 999)
+        {
+            num /= 10;
+                if (num < 1000)
+                {
+                    figure3 = num % 10;
+                    Console.WriteLine(figure3);
+                    break;
+                }
+        }
+}
+else if (num > 99)
+{
+    figure3 = num % 10;
+    Console.WriteLine(figure3);
+}
+else if (num > -99 || num < 100)
+{
+    Console.WriteLine("У вашего числа нет третьей цифры");
+}
