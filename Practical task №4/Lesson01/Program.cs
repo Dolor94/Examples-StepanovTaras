cb91a33954b1d8db8a402e9723291e0dc4a5dbae
Dolor <taras_stepanov@mail.ru>
2022-07-10 14:20:55 +0500

    Homework - Task №25

--- Practical task №4/Lesson01/Program.cs
+++ Practical task №4/Lesson01/Program.cs
@@ -3,8 +3,25 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+// Решение через встроенную функцию:
+
+// Console.WriteLine("Программа возводящая число А в степень В!");
+// Console.Write("Введите число А: ");
+// double number = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите число В: ");
+// double power = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"{number}^{power} = " + Math.Pow(number, power));
+
+// Решение через цикл:
+
+Console.WriteLine("Программа возводящая число А в степень В!");
 Console.Write("Введите число А: ");
-double number = Convert.ToDouble(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число В: ");
-double power = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"{number}^{power} = " + Math.Pow(number, power));
+int power = Convert.ToInt32(Console.ReadLine());
+int newNumber = number;
+for (int index = 2; index <= power; index++)
+{
+    newNumber *= number;
+}
+Console.WriteLine($"{number}^{power} = {newNumber}");
